b85e1833b0659815ec3a87f60e01eadb06160019
kosmiyn <119161875+kosmiyn@users.noreply.github.com>
2024-09-09 20:28:50 +0300

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,18 @@ if status is-interactive
 alias vim nvim
 alias ls lsd
 alias unzip 'unzip -q'
-alias cat bat
-set fish_greeting 
+set fish_greeting
 fastfetch --logo /home/kosmiyn/.config/fastfetch/cat
 end
 
 fish_add_path /home/kosmiyn/.spicetify
+
+# pnpm
+set -gx PNPM_HOME "/home/kosmiyn/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# cevaceva
+function pkgsearch
+	pacman -Slq | fzf --preview 'pacman -Si {}' --layout=reverse
+end
